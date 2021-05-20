4e51fc8951583ca7dbeca8346bca41a4aeecaf9c
Pratik44 <pratik.deenbandhu456@gmail.com>
2021-05-21 00:09:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
-
-#module "vpc" {
-#   source = "/Users/apple/Desktop/Terraform/terraform-poc/lib.aws.vpc/"
-#}
+module "vpc" {
+   source = "git::git@github.com:Pratik44/lib.aws.vpc"
+}
 
 
 
 
 resource "aws_subnet" "public_sub" {
-    vpc_id = "${var.vpc_id}"
+    vpc_id = "${module.vpc.vpc_id}"
     cidr_block = "10.0.1.0/24"
     tags = {
         Name = "Public-subnet"
@@ -15,7 +14,7 @@ resource "aws_subnet" "public_sub" {
 }
 
 resource "aws_subnet" "private_sub" {
-    vpc_id = "${var.vpc_id}"
+    vpc_id = "${module.vpc.vpc_id}"
     cidr_block = "10.0.2.0/24"
     tags = {
         Name = "Private-subnet"
